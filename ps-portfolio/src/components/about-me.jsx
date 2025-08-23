36b07de4d420a4df9cccf05ed632a388c91a0c4a
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-23 14:24:43 +0200

    wip: more cleaning up

--- ps-portfolio/src/components/about-me.jsx
+++ ps-portfolio/src/components/about-me.jsx
@@ -22,20 +22,13 @@ export default function AboutMe() {
                     <h1>Hi, I'm Sbu Ndhlovu</h1>
                     <p>
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo aperiam assumenda ut veritatis ducimus autem sed unde eaque! Consequatur, nobis q
-                        uam provident aliquid ullam cupiditate quod nisi quas illo delectus? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo aperiam assumenda 
+                        uam provident aliquid ullam cupiditate quod nisi quas illo delectus? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo aperiam assumenda
                         ut veritatis ducimus autem sed unde eaque! Consequatur, nobis q uam provident aliquid ullam cupiditate quod nisi quas illo delectus?
                     </p>
                     <div className='about-me-container-buttons'>
                         <button className='btn-primary'>Download CV</button>
                         <button className='btn-primary'>Projects</button>
                         <button className='btn-primary'>Get In Touch</button>
-                    </div>
-                    <div className='about-me-container-socials' >
-                        <ul>
-                            <li><a href="#"><img src='https://img.icons8.com/?size=100&id=sbhfmWq4KRr1&format=png&color=000000' /></a></li>
-                            <li><a href="#"><img src='https://img.icons8.com/?size=100&id=fFbCvS8wxZFo&format=png&color=000000' /></a></li>
-                            <li><a href="#"><img src='https://img.icons8.com/?size=100&id=67570&format=png&color=000000' /></a></li>
-                        </ul>
                     </div>
                 </div>
                 <div className='about-me-container-avatar'>
